667ef19c06c4e4e9a139965162cd5fc55f9209d0
Zamor <przemyslaw.zamorskii@gmail.com>
2016-05-30 08:13:43 +0200

    komunikaty błędów

--- login.js
+++ login.js
@@ -1,10 +1,11 @@
 function check_server() {
     window.serwer = localStorage.getItem("server");
+    window.serwer_display = localStorage.getItem("server_name");
     if (window.serwer) {
         $("#serwer_name").empty();
         $("#login_form").css("display", "block");
         $("#serwer_form").css("display", "none");
-        $("#serwer_name").append(window.serwer);
+        $("#serwer_name").append(window.serwer_display);
     } else {
         $("#serwer_form").css("display", "block");
         $("#login_form").css("display", "none");
@@ -12,12 +13,82 @@ function check_server() {
 }
 
 function add_serwer() {
+    $("#server_error").css("display", "none");
+    $("#no_server").css("display", "none");
     localStorage.removeItem('server');
+    localStorage.removeItem('server_name');
     window.serwer_data = $("#serwer_form").serializeArray();
+    /*nazwa serwera*/
     var server_name = serwer_data[0].value;
-    localStorage.setItem("server", server_name);
-    check_server();
-    $("#load_assign_gif").css("display", "none");
+
+
+    if (server_name.indexOf("https://") > -1) {
+        localStorage.setItem("server", server_name);
+        localStorage.setItem("server_name", server_name);
+        console.log(server_name);
+        check_server();
+        $("#load_assign_gif").css("display", "none");
+        var index = window.serwer.indexOf("/");
+        index = index + 1;
+        window.header = window.serwer.substr(0, index);
+        window.rest_url = window.serwer.substr(index + 1);
+
+
+    } else {
+        $.ajax({
+            type: 'GET',
+            contentType: "text/xml",
+            url: "http://www.fastdata.com.pl/kontrahenci/config.xml",
+            success: function (data) {
+                url_port = 0;
+                console.log(data);
+                var kontrahents = data.getElementsByTagName("kontrahent");
+                var server_name_upper = server_name.toUpperCase();
+
+                /*przeszukuje kontrahentow w wyszukiwaniu kodu */
+                for (var i = 0; i < kontrahents.length; i++) {
+
+                    if (server_name_upper == kontrahents[i].getElementsByTagName("kod")[0].innerHTML) {
+                        url_port = kontrahents[i].getElementsByTagName("port")[0].innerHTML;
+                    }
+
+                }
+
+                /*jezelei istnieje taki kod to*/
+                if (url_port) {
+                    var full_server_url = "https://" + server_name_upper + ".fastdata.com.pl:" + url_port;
+                    console.log(full_server_url);
+                    localStorage.setItem("server", full_server_url);
+                    localStorage.setItem("server_name", server_name);
+                    check_server();
+                    $("#load_assign_gif").css("display", "none");
+                    var index = window.serwer.indexOf("/");
+                    index = index + 1;
+                    window.header = window.serwer.substr(0, index);
+                    window.rest_url = window.serwer.substr(index + 1);
+
+
+                } else {
+                    $("#no_server").css("display", "block");
+
+                }
+
+
+            },
+            error: function (xhr, err) {
+                $("#server_error").css("display", "block");
+
+            }
+        });
+    }
+
+
+    /*check_server();
+     $("#load_assign_gif").css("display", "none");
+     var index = window.serwer.indexOf("/");
+     index = index + 1;
+     window.header = window.serwer.substr(0, index);
+     window.rest_url = window.serwer.substr(index + 1);*/
 }
 
 function change_serwer() {
@@ -50,11 +121,7 @@ function log_in() {
         window.password = "a";
     }
 
-    var serwer_url = window.serwer;
-    var index = serwer_url.indexOf("/");
-    index = index + 1;
-    window.header = serwer_url.substr(0, index);
-    window.rest_url = serwer_url.substr(index + 1);
+
     $.ajax({
         type: 'GET',
         async: true,
@@ -73,7 +140,7 @@ function log_in() {
             $("#login").css('display', "none");
             $("#contact_info_load").remove();
         },
-        error: function (xhr,err) {
+        error: function (xhr, err) {
             console.log("nie autoryzowano");
             $("#login_error").css("display", "block");
 
